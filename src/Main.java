5f76cf2fa1e2f4bbff7ccc9b12f145ed9947538e
matiullahmalikzadah <matiullah198611@gmail.com>
2024-07-28 21:38:06 -0700

    firts commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("jOOOOOOOON");
+        System.out.println("Khannnnnnnnn");
+        System.out.println("zoooooom");
+    }
+}
